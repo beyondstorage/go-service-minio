ce9654d99c559d6f87a13cb6aa43f1b15f0f4f1e
Wei <aeinrw@gmail.com>
2021-07-05 15:31:58 +0800

    Modify test file

--- tests/utils_test.go
+++ tests/utils_test.go
@@ -14,10 +14,25 @@ import (
 func setupTest(t *testing.T) types.Storager {
 	t.Log("Setup test for minio")
 
+	srv, err := minio.NewServicer(
+		ps.WithCredential(os.Getenv("STORAGE_MINIO_CREDENTIAL")),
+		ps.WithEndpoint(os.Getenv("STORAGE_MINIO_ENDPOINT")),
+	)
+	if err != nil {
+		t.Errorf("new servicer: %v", err)
+	}
+
+	bucketName := os.Getenv("STORAGE_MINIO_NAME")
+
+	_, err = srv.Create(bucketName)
+	if err != nil {
+		t.Errorf("create storager: %v", err)
+	}
+
 	store, err := minio.NewStorager(
 		ps.WithCredential(os.Getenv("STORAGE_MINIO_CREDENTIAL")),
 		ps.WithEndpoint(os.Getenv("STORAGE_MINIO_ENDPOINT")),
-		ps.WithName(os.Getenv("STORAGE_MINIO_NAME")),
+		ps.WithName(bucketName),
 		ps.WithWorkDir("/"+uuid.New().String()),
 	)
 	if err != nil {
@@ -26,6 +41,11 @@ func setupTest(t *testing.T) types.Storager {
 
 	t.Cleanup(func() {
 		err = store.Delete("")
+		if err != nil {
+			t.Errorf("cleanup: %v", err)
+		}
+
+		err = srv.Delete(bucketName)
 		if err != nil {
 			t.Errorf("cleanup: %v", err)
 		}
